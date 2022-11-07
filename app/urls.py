69cae337ae894ce2d99148316d8c8040f9c9743e
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-07 21:25:34 +0300

    created mock api data

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 # importing of the views from
 urlpatterns =[
-    path("", views.getRoutes, name="routes")
+    path("", views.getRoutes, name="routes"),
+    path("products/", views.getProducts, name="products")
 ]
