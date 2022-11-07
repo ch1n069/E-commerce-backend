8e41e9b53ec16d08b45055d58d8cfd157989a4a6
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-07 21:37:19 +0300

    added view function to get a single product

--- app/views.py
+++ app/views.py
@@ -2,11 +2,27 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .products import products
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
+# @api_view(['GET'])
 def getRoutes(request):
     return JsonResponse('Hello', safe=False)
 
-
+@api_view(['GET'])
 def getProducts(request):
-    return JsonResponse(products , safe=False)
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request,pk):
+    product = None
+    # get a single product first loop through all products
+    for i in products:
+        if i ['_id'] == pk:
+            product = i
+            break
+        
+    return Response(product)
+
+
