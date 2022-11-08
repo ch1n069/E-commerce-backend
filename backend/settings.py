dcf41dee3998652a6db326bb015c24121c60b5b0
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-08 10:24:39 +0300

    added cors header

--- backend/settings.py
+++ backend/settings.py
@@ -39,9 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'rest_framework',
+    "corsheaders",
+
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    'django.middleware.common.CommonMiddleware',
+
+
+    
+    
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +132,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS_ORIGIN_WHITELIST = [
+# 'http://localhost:3000',
+# 'http://127.0.0.1:3000'
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
+
