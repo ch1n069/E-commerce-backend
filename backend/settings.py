1e64b7efbbed5ca346a0c8a977a9e7dd37921a25
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-08 11:05:35 +0300

    created the products model

--- backend/settings.py
+++ backend/settings.py
@@ -86,11 +86,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'duka',                      
+        'USER': 'bruno',
+        'PASSWORD': 'newpassword',
+        'HOST': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
