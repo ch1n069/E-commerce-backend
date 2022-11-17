6f7b37df7928d14219f21dcfb6f518f43cf58a22
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-17 13:05:57 +0300

    customized the return from the user model

--- app/serializers.py
+++ app/serializers.py
@@ -3,6 +3,27 @@ from django.contrib.auth.models import User
 from .models import Product
 
 
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    createdOn = serializers.SerializerMethodField(read_only=True)
+    class Meta: 
+        model = User
+        
+        fields = ['id','username', 'email','name', "createdOn"]
+        
+    def get_name(self , obj):
+        name = obj.first_name
+        if name == '':#if user has not set name use email instead
+            name = obj.email
+            
+        return name
+    def get_createdOn(self, obj):
+        createdOn = obj.date_joined
+        return createdOn
+    
+    
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Product
--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer , UserProfileSerializer
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -29,6 +29,14 @@ class MyTokenObtainPairView(TokenObtainPairView):
 # @api_view(['GET'])
 def getRoutes(request):
     return JsonResponse('Hello', safe=False)
+
+
+@api_view(['GET'])
+def getUserProfile(request):
+    
+    user =request.user
+    serializer = UserProfileSerializer(user, many=False)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProducts(request):
