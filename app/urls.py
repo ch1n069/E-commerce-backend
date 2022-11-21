f6af5e4cd6d113734a886a73f6f6530bf99e8ea1
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-21 23:43:06 +0300

    created url for updating the profile

--- app/urls.py
+++ app/urls.py
@@ -7,11 +7,13 @@ from rest_framework_simplejwt.views import (
 
 # importing of the views from
 urlpatterns =[
-    path('users/login',  views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/',  views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 
     path("", views.getRoutes, name="routes"),
     path("users/register/", views.registerUser, name="register"),
     path("users/profile/", views.getUserProfile, name="user-profile"),
+        path("users/profile/update/", views.updateUserProfile, name="user-profile-update"),
+
     path("users/", views.getAllUsers, name="users"),
 
     path("products/", views.getProducts, name="products"),
