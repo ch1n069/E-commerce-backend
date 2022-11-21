9b3b4d3620aa23d457d895317782b5f93a4c9ed6
Bruno Alpachino <brunolalpachino@gmail.com>
2022-11-21 23:43:29 +0300

    created route to update the user profile

--- app/views.py
+++ app/views.py
@@ -70,6 +70,28 @@ def registerUser(request):
 
 # END OF USER REGISTER VIEW 
 
+#//////////////////////////////// users edit profile //////////////////////////////////
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    
+    user =request.user
+    serializer = UserSerializerWithToken(user, many=False)
+    
+    data = request.data #pulling out the data
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    
+    if data['password'] != "":
+        user.password = make_password(data['password'])
+    user.save()
+    return Response(serializer.data)
+
+
+
 
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
